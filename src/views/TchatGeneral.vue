4942db6717700824077b863c4ea42c2d2f7a6cb9
adrienphilippot <adrienphilippot@gmail.com>
2021-04-27 09:52:18 +0200

    feat: add header, input, row de test

--- src/views/TchatGeneral.vue
+++ src/views/TchatGeneral.vue
@@ -1,28 +1,43 @@
+<style>
+.ion-avatar{
+  padding-left: 1em;
+}
+.ion-label{
+  padding-left: 1em;
+}
+.ion-input{
+  border: 1px solid #ffb0b0;
+  border-radius: 4px;
+  position: fixed;
+  bottom: 60px;
+  width: 100%;
+}
+</style>
+
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tchat général</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+    <ion-content>
+      <ion-list>
+        <ion-list-header lines="full">
+          <ion-label>SuP€r-M€$$@g3R-3003x</ion-label>
+        </ion-list-header>
+        <ion-item>
+          <ion-avatar class="ion-avatar"><img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" /></ion-avatar>
+          <ion-label class="ion-label">SuperTesteur</ion-label>
+        </ion-item>
+      </ion-list>
+      <ion-item class="ion-input">
+        <ion-input placeholder="Message texte"></ion-input>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonContent, IonItem, IonList, IonLabel, IonAvatar, IonInput } from '@ionic/vue';
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonContent, IonPage, IonItem, IonList, IonLabel, IonAvatar, IonInput }
 }
 </script>
